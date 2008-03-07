9510bcd5759fe2a7cb6e4289b68b5a57ceb83427
Govind Salinas <blix@sophiasuchtig.com>
2008-03-06 20:09:15 -0600

    repository.py: Clean up python style.
    Signed-off-by: Govind Salinas <blix@sophiasuchtig.com>

--- pyrite/repository.py
+++ pyrite/repository.py
@@ -21,14 +21,19 @@ class RepoError(Exception):
 
 class Repo(object):
     def __init__(self, location=None):
-        if location: self._location = os.path.expanduser(location)
-        else: self._location = os.getcwd()
+        if location:
+            self._location = os.path.expanduser(location)
+        else:
+            self._location = os.getcwd()
         self.refresh()
         
     def _popen(self, args, cwd=None, stdin=False):
-        if not cwd: cwd = self._location
-        if stdin: stdin = subprocess.PIPE
-        else: stdin = None
+        if not cwd:
+            cwd = self._location
+        if stdin:
+            stdin = subprocess.PIPE
+        else:
+            stdin = None
         return subprocess.Popen(args, cwd=cwd,
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE,
@@ -53,22 +58,27 @@ class Repo(object):
         return self._is_repo
         
     def validate(self):
-        if not self._is_repo: raise RepoError(_('Not under a repo')) 
+        if not self._is_repo:
+            raise RepoError(_('Not under a repo')) 
 
     def init(self):
         proc = self._popen(('git', 'init'), cwd=self._location)
         if proc.wait():
-            raise RepoError(_('Failed to init repo'))
+            raise RepoError(_('Failed to init repo: %s') % proc.stderr.read())
 
     def del_branch(self, names, force):
     #TODO: need to give better error when branch exists
         self.validate()
         args = ['git', 'branch']
-        if force: args.append('-D')
-        else: args.append('-d')
+        if force:
+            args.append('-D')
+        else:
+            args.append('-d')
         for n in names: args.append(n)
         proc = self._popen(args)
-        if proc.wait(): raise RepoError(_('Failed to delete branch %s') % names)
+        if proc.wait():
+            raise RepoError(_('Failed to delete branch %s: %s') %
+                         (names, proc.stderr.read()))
 
     def rename_branch(self, oldname, newname, force):
     #TODO: need to give better error when branch exists
@@ -79,39 +89,49 @@ class Repo(object):
         else:
             proc = self._popen(('git', 'branch', '-m', oldname, newname))
         if proc.wait():
-            raise RepoError(_('Failed to rename branch %s to %s') % (oldname,
-                                newname))
+            raise RepoError(_('Failed to rename branch %s to %s: %s') %
+                                (oldname, newname, proc.stderr.read()))
 
     def create_branch(self, name, force, track=True):
         self.validate()
         args = ['git', 'branch']
-        if force: args.append('-f')
-        if track: args.append('--track')
-        else: args.append('--no-track')
+        if force:
+            args.append('-f')
+        if track:
+            args.append('--track')
+        else:
+            args.append('--no-track')
         args.append(name)
         proc = self._popen(args)
         if proc.wait():
             if not force and name in self.branches():
                  raise RepoError(_('Could not create branch, branch exists'))
-            else: raise RepoError(_('Could not create branch'))
+            else:
+                raise RepoError(_('Could not create branch: %s') %
+                                    proc.stderr.read())
 
     def checkout(self, commit, is_merge, paths=None):
         self.validate()
         args = ['git', 'checkout']
-        if is_merge: args.append('-m')
+        if is_merge:
+            args.append('-m')
         args.append(commit)
         if paths: args.extend(paths)
         proc = self._popen(args)
-        if proc.wait(): raise RepoError(_('Failed to switch to %s'), commit)
+        if proc.wait():
+             raise RepoError(_('Failed to switch to %s: %s') % (commit,
+                                proc.stderr.read()))
         
     def _read_remotes(self):
         self._remotes = []
         if not self.is_repo:
             return
         proc = self._popen(('git', 'branch', '-r'))
-        if proc.wait(): raise RepoError('Could not get remotes list')
         for b in proc.stdout.readlines():
              self._remotes.append(b.strip())
+        if proc.wait():
+            raise RepoError('Could not get remotes list: %s' %
+                            proc.stderr.read())
 
     def _read_branches(self):
         self._branches = []
@@ -119,13 +139,15 @@ class Repo(object):
         if not self.is_repo:
             return
         proc = self._popen(('git', 'branch'))
-        if proc.wait(): raise RepoError('Could not get branch list')
         for b in proc.stdout.readlines():
             tokens = b.split()
             if len(tokens) == 2:
                 self._current_branch = tokens[1].strip()
                 self._branches.append(self._current_branch)
             else: self._branches.append(tokens[0].strip())
+        if proc.wait():
+            raise RepoError(_('Could not get branch list: %s') %
+                                proc.stderr.read())
 
     def branches(self):
         self.validate()
@@ -154,14 +176,17 @@ class Repo(object):
     def get_head_sha(self):
         self.validate()
         proc = self._popen(('git', 'rev-list', '--max-count=1', 'HEAD'))
-        if proc.wait(): raise RepoError(_('Count not find HEAD sha'))
+        if proc.wait():
+            raise RepoError(_('Count not find HEAD sham: %s') %
+                                proc.stderr.read())
         return proc.stdout.readline().strip()
 
     def get_commit_info(self, commit):
         self.validate()
         proc = self._popen(('git', 'rev-list', '--max-count=1',
                         '--pretty=format:%an <%ae>%n%s%n%b', commit))
-        if proc.wait(): return None, None, None
+        if proc.wait():
+            return None, None, None
         lines = proc.stdout.readlines()
         if len(lines) < 3: return None, None, None
         return ''.join(lines[2:]), lines[1].strip()
@@ -178,7 +203,9 @@ class Repo(object):
         else:
             args.append('-u')
         proc = self._popen(args)
-        if proc.wait(): raise RepoError(_('Failed to update index'))
+        if proc.wait():
+            raise RepoError(_('Failed to update index: %s') %
+                                proc.stderr.read())
 
     def changed_files(self, commit=None):
         self.validate()
@@ -189,16 +216,19 @@ class Repo(object):
         else:
             proc = self._popen(('git', 'diff-index', '--cached',
                                 '--name-status', 'HEAD'))
-        if proc.wait(): raise RepoError(_('Failed to get changed files'))
         for line in proc.stdout.readlines():
             parts = line.split()
             yield parts[0], parts[1]
+        if proc.wait():
+            raise RepoError(_('Failed to get changed files: %s') %
+                                proc.stderr.read())
 
     def commit(self, use_message, use_author, verify=True, commit=None,
                 paths=None):
         self.validate()
         args = ['git', 'commit']
-        if not verify: args.append('--no-verify')
+        if not verify:
+            args.append('--no-verify')
         if use_author:
             args.append('--author')
             args.append(use_author)
@@ -218,10 +248,13 @@ class Repo(object):
     def gc(self, prune, aggressive):
         self.validate()
         args = ['git', 'gc']
-        if prune: args.append('--prune')
-        if aggressive: args.append('--aggressive')
+        if prune:
+            args.append('--prune')
+        if aggressive:
+            args.append('--aggressive')
         proc = self._popen(args)
-        if proc.wait(): raise RepoError(_('Failed to gc'))
+        if proc.wait():
+            raise RepoError(_('Failed to gc: %s') % proc.stderr.read())
 
     def verify(self, verbose):
         self.validate()
@@ -229,22 +262,27 @@ class Repo(object):
             proc = self._popen(('git', 'fsck', '--verbose'))
         else:
             proc = self._popen(('git', 'fsck'))
+        for line in proc.stdout.readlines():
+            yield line
         if proc.wait():
-            raise RepoError(_('Verify failed.'))
-        return proc.stdout.readlines()
+            raise RepoError(_('Verify failed: %s') % proc.stderr.read())
 
     def add_files(self, is_force, is_verbose, files):
         self.validate()
         args = ['git', 'add']
-        if is_force: args.append('-f')
-        if is_verbose: args.append('-v')
-        if len(files) > 0: args.extend(files)
-        else: args.append('.')
+        if is_force:
+            args.append('-f')
+        if is_verbose:
+            args.append('-v')
+        if len(files) > 0:
+            args.extend(files)
+        else:
+            args.append('.')
         proc = self._popen(args, cwd=os.getcwd())
-        if proc.wait():
-            raise RepoError(_('Could not add files'))
         for line in proc.stdout.readlines():
             yield line
+        if proc.wait():
+            raise RepoError(_('Could not add files: %s') % proc.stderr.read())
             
     def list_tags(self, pattern):
         self.validate()
@@ -253,29 +291,29 @@ class Repo(object):
             proc = self._popen(('git', 'tag', '-l', pattern))
         else:
             proc = self._popen(('git', 'tag', '-l'))
-        if proc.wait():
-            raise RepoError(_('Failed to list tags'))
         for line in proc.stdout.readlines():
             yield line
+        if proc.wait():
+            raise RepoError(_('Failed to list tags: %s') % proc.stderr.read())
 
     def verify_tag(self, tag):
         self.validate()
         proc = self._popen(('git', 'tag', '-v', tag))
-        if proc.wait():
-            raise RepoError(_('Failed to verify tag'))
         for line in proc.stdout.readlines():
             yield line
-        for line in proc.stderr.readlines():
-            yield line
+        if proc.wait():
+            raise RepoError(_('Failed to verify tag: %s') % proc.stderr.read())
 
     def delete_tags(self, tags):
         self.validate()
         args = ['git', 'tag', '-d']
         args.extend(tags)
         proc = self._popen(args)
+        for line in proc.stdout.readlines():
+            yield line
         if proc.wait():
-            raise RepoError(_('Failed to delete tag(s)'))
-        return proc.stdout.readlines()
+            raise RepoError(_('Failed to delete tag(s): %s') %
+                            proc.stderr.read())
 
     def create_tag(self, name, message, key=None, sign=False):
         self.validate()
@@ -289,39 +327,50 @@ class Repo(object):
         elif sign: args.append('-s')
         args.append(name)
         proc = self._popen(args)
+        for line in proc.stdout.readlines():
+            yield line
         if proc.wait():
-            raise RepoError(_('Failed to create tag'))
-        return proc.stdout.readlines()
+            raise RepoError(_('Failed to create tag: %s') % proc.stderr.read())
 
     def push(self, repo, source, target, force=False, all_branches=False,
                 all_tags=False, verbose=False):
         self.validate()
         args = ['git', 'push']
-        if force: args.append('f')
-        if all_branches: args.append('--all')
-        if all_tags: args.append('--tags')
-        if verbose: args.append('-v')
+        if force:
+            args.append('f')
+        if all_branches:
+            args.append('--all')
+        if all_tags:
+            args.append('--tags')
+        if verbose:
+            args.append('-v')
         args.append(repo)
         if source: 
             arg = source
             if target: arg += ':' + target
             args.append(arg)
         proc = self._popen(args)
-        if proc.wait():
-            raise RepoError(_('Failed to push'))
         for line in proc.stdout.readlines():
             yield line
+        if proc.wait():
+            raise RepoError(_('Failed to push: %s') % proc.stderr.read())
             
     def pull(self, repo, source, target, force=False, tags="normal",
                 commit=True, depth=-1, rebase=False):
         self.validate()
         args = ['git', 'pull']
-        if force: args.append('-f')
-        if tags == 'none': args.append('--no-tags')
-        elif tags == 'extra': args.append('--tags')
-        if depth > -1: args.append('--depth=%d' % depth)
-        if not commit: args.append('--no-commit')
-        if rebase: args.append('--rebase')
+        if force:
+            args.append('-f')
+        if tags == 'none':
+            args.append('--no-tags')
+        elif tags == 'extra':
+            args.append('--tags')
+        if depth > -1:
+            args.append('--depth=%d' % depth)
+        if not commit:
+            args.append('--no-commit')
+        if rebase:
+            args.append('--rebase')
         args.append(repo)
         if source: 
             arg = source
@@ -329,47 +378,57 @@ class Repo(object):
             args.append(arg)
 
         proc = self._popen(args)
-        if proc.wait():
-            print '\n'.join(proc.stderr.readlines())    
-            raise RepoError(_('Failed to pull'))
         for line in proc.stdout.readlines():
             yield line
+        if proc.wait():
+            raise RepoError(_('Failed to pull: %s') % proc.stderr.read())
 
     def clone(self, repo, directory, bare=False, checkout=True, depth=-1):
         args = ['git', 'clone']
-        if bare: args.append('--bare')
-        if not checkout: args.append('--no-checkout')
-        if depth > -1: args.append('--depth=%d' % depth)
+        if bare:
+            args.append('--bare')
+        if not checkout:
+            args.append('--no-checkout')
+        if depth > -1:
+            args.append('--depth=%d' % depth)
         args.append(repo)
-        if directory: args.append(directory)
+        if directory:
+            args.append(directory)
 
         proc = self._popen(args)
-        if proc.wait():
-            raise RepoError(_('Failed to clone'))
         for line in proc.stdout.readlines():
             yield line
-
+        if proc.wait():
+            raise RepoError(_('Failed to clone: %s') % proc.stderr.read())
+        
     def diff(self, start, end, paths, stat=False, color=False, template=None,
                 detect=False, ignorewhite='none'):
         self.validate()
         args = ['git', 'diff', '--stat']
-        if not stat: args.append('-p')
-        if color: args.append('--color')
-        if detect: args.extend(['-B', '-M', '-C'])
-        if ignorewhite == 'all': args.append('-w')
-        elif ignorewhite == 'eol': args.append('--ignore-space-at-eol')
+        if not stat:
+            args.append('-p')
+        if color:
+            args.append('--color')
+        if detect:
+            args.extend(['-B', '-M', '-C'])
+        if ignorewhite == 'all':
+            args.append('-w')
+        elif ignorewhite == 'eol':
+            args.append('--ignore-space-at-eol')
         spec = start
-        if not start: spec = 'HEAD'
-        elif end: spec = start + '..' + end
+        if not start:
+            spec = 'HEAD'
+        elif end:
+            spec = start + '..' + end
         args.append(spec)
         if paths:
             args.append('--')
             args.extend(paths)
         proc = self._popen(args)
-        if proc.wait():
-            raise RepoError(_('Failed to diff'))
         for line in proc.stdout.readlines():
             yield line
+        if proc.wait():
+            raise RepoError(_('Failed to diff: %s') % proc.stderr.read())
 
     def list(self):
         self.validate()
@@ -383,6 +442,7 @@ class Repo(object):
             status = item[0]
             filename = item[2:]
             files[filename] = Repo.Status[status]
+        proc.wait()
         return files
 
     def _convert_range(self, first, last):
@@ -405,15 +465,14 @@ class Repo(object):
             args.append('-' + str(limit))
         #if show_patch: args.append('-p')
         # Ignore patch parameter for now to simplfy the parse
-        if follow: args.append('--follow')
+        if follow:
+            args.append('--follow')
         if first:
             args.append(self._convert_range(first, last))
         if paths:
             args.append('--')
             args.extend(paths)
         proc = self._popen(args)
-        if proc.wait():
-            raise RepoError(_('Failed to get log'))
         for line in proc.stdout.readlines():
             idx = line.find(' ')
             ID = line[:idx]
@@ -421,12 +480,15 @@ class Repo(object):
             parents = line[idx+1:idx2].split(' ')
             name, email, date, subj = line[idx2+1:].split('\t')
             yield ID, parents, name, email, date, subj
+        if proc.wait():
+            raise RepoError(_('Failed to get log: %s') % proc.stderr.read())
 
     def merge(self, branch, show_summary=False, merge_strategy=None,
                 message=None):
         self.validate()
         args = ['git', 'merge']
-        if show_summary: args.append('--summary')
+        if show_summary:
+            args.append('--summary')
         if merge_strategy:
             args.append('-s')
             args.append(merge_strategy)
@@ -435,10 +497,10 @@ class Repo(object):
             args.append(message)
         args.append(branch)
         proc = self._popen(args)
-        if proc.wait():
-            raise RepoError(_('Failed to merge: %s') % proc.stderr.read())
         for line in proc.stdout.readlines():
             yield line
+        if proc.wait():
+            raise RepoError(_('Failed to merge: %s') % proc.stderr.read())
 
     def export_patch(self, first, last, outdir, force=False, numbered=False):
         self.validate()
@@ -474,3 +536,4 @@ class Repo(object):
                 pass
             return False, proc.stderr.readlines()
         return True, None
+
