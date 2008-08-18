1eca384a38ca25e12345466539646bacc8bf7885
Govind Salinas <blix@sophiasuchtig.com>
2008-08-18 00:04:56 -0500

    Blame web template and other improvements.
    Add blame view from the file list.
    Improve the tree view by adding commit information.
    Improve color scheme.
    Tag and Head labels now take you to that ref.
    
    Factor out some common code to a utils template.
    
    Signed-off-by: Govind Salinas <blix@sophiasuchtig.com>

--- pyrite/templates/default-web/__config__.py
+++ pyrite/templates/default-web/__config__.py
@@ -3,10 +3,10 @@ url_map = (
     (r'^$', 'project.html'),
     (r'^(?P<project>[^/]*)/?(?P<page>[0-9]*)$', 'project.html'),
     (r'^(?P<project>.*)/commit/(?P<commit_id>.*)$', 'commit.html'),
-    (r'^(?P<project>.*)/tree/(?P<tree_id>.*)\?path=(?P<path>.*)$',
-        'tree.html'),
-    (r'^(?P<project>.*)/blob/(?P<blob_id>.*)\?path=(?P<path>.*)$',
-     'blob.html'),
+    (r'^(?P<project>.*)/tree/(?P<commit_id>[a-z0-9]*)/(?P<tree_id>.*)'
+        '\?path=(?P<path>.*)$', 'tree.html'),
+    (r'^(?P<project>.*)/blob/(?P<commit_id>[a-z0-9]*)/(?P<blob_id>.*)'
+     '\?path=(?P<path>.*)$', 'blob.html'),
     (r'^(?P<project>.*)/ref/(?P<ref_id>[^/]*)/?(?P<page>[0-9]*)$',
                                                         'ref.html'),
     (r'^(?P<project>.*)/diff/(?P<v1>[a-z0-9]*)/(?P<v2>[a-z0-9]*)/(?P<f>.*)$',
@@ -14,6 +14,8 @@ url_map = (
     (r'^(?P<project>.*)/ssdiff/(?P<v1>.*)/(?P<v2>.*)/(?P<f>.*)$',
                                                         'ssdiff.html'),
     (r'^(?P<project>.*)/reflist/(?P<type>.*)$', 'all_type_refs.html'),
+    (r'^(?P<project>.*)/blame/(?P<commit_id>[a-z0-9]*)'
+        '\?path=(?P<path>.*)$', 'blame.html'),
     # add line for tag viewer ()
 )
 
