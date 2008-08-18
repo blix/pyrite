cafd103d50fb964ff50766645178f707b67eb666
Govind Salinas <blix@sophiasuchtig.com>
2008-08-17 23:52:15 -0500

    Improvements to web templates.
    This change adds paging to commit tables, a all-tags or
    all-branches page, updates to the style to make it look nicer
    and show tags on commits in the commit tables.
    
    Signed-off-by: Govind Salinas <blix@sophiasuchtig.com>

--- pyrite/templates/default-web/__config__.py
+++ pyrite/templates/default-web/__config__.py
@@ -1,16 +1,19 @@
 
 url_map = (
     (r'^$', 'project.html'),
+    (r'^(?P<project>[^/]*)/?(?P<page>[0-9]*)$', 'project.html'),
     (r'^(?P<project>.*)/commit/(?P<commit_id>.*)$', 'commit.html'),
     (r'^(?P<project>.*)/tree/(?P<tree_id>.*)\?path=(?P<path>.*)$',
         'tree.html'),
     (r'^(?P<project>.*)/blob/(?P<blob_id>.*)\?path=(?P<path>.*)$',
      'blob.html'),
-    (r'^(?P<project>.*)/ref/(?P<ref_id>.*)$', 'ref.html'),
-    (r'^(?P<project>[^/]*)$', 'project.html'),
+    (r'^(?P<project>.*)/ref/(?P<ref_id>[^/]*)/?(?P<page>[0-9]*)$',
+                                                        'ref.html'),
     (r'^(?P<project>.*)/diff/(?P<v1>[a-z0-9]*)/(?P<v2>[a-z0-9]*)/(?P<f>.*)$',
                                                         'diff.html'),
     (r'^(?P<project>.*)/ssdiff/(?P<v1>.*)/(?P<v2>.*)/(?P<f>.*)$',
                                                         'ssdiff.html'),
+    (r'^(?P<project>.*)/reflist/(?P<type>.*)$', 'all_type_refs.html'),
+    # add line for tag viewer ()
 )
 
