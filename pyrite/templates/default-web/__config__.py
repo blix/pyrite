be430eb09e02d1a80e66dde79310cba99a08e3f8
Govind Salinas <blix@sophiasuchtig.com>
2008-08-18 00:46:18 -0500

    Use show to display blob web template.
    Using show requires less information in the get string.
    
    Signed-off-by: Govind Salinas <blix@sophiasuchtig.com>

--- pyrite/templates/default-web/__config__.py
+++ pyrite/templates/default-web/__config__.py
@@ -5,8 +5,8 @@ url_map = (
     (r'^(?P<project>.*)/commit/(?P<commit_id>.*)$', 'commit.html'),
     (r'^(?P<project>.*)/tree/(?P<commit_id>[a-z0-9]*)/(?P<tree_id>.*)'
         '\?path=(?P<path>.*)$', 'tree.html'),
-    (r'^(?P<project>.*)/blob/(?P<commit_id>[a-z0-9]*)/(?P<blob_id>.*)'
-     '\?path=(?P<path>.*)$', 'blob.html'),
+    (r'^(?P<project>.*)/blob/(?P<commit_id>[a-z0-9]*)/(?P<path>.*)$',
+                                                        'blob.html'),
     (r'^(?P<project>.*)/ref/(?P<ref_id>[^/]*)/?(?P<page>[0-9]*)$',
                                                         'ref.html'),
     (r'^(?P<project>.*)/diff/(?P<v1>[a-z0-9]*)/(?P<v2>[a-z0-9]*)'
