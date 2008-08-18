4d2087f348083642a60e10b26027852a2e4db7bc
Govind Salinas <blix@sophiasuchtig.com>
2008-08-17 23:27:25 -0500

    Improve web templates.
    This change adds many impprovement to the web templates
    including utf-8 including through Pygments, showing a list of
    changed files in a commit, with links to the diffs (side by
    side not yet functional).
    
    Signed-off-by: Govind Salinas <blix@sophiasuchtig.com>

--- pyrite/templates/default-web/__config__.py
+++ pyrite/templates/default-web/__config__.py
@@ -2,10 +2,15 @@
 url_map = (
     (r'^$', 'project.html'),
     (r'^(?P<project>.*)/commit/(?P<commit_id>.*)$', 'commit.html'),
-    (r'^(?P<project>.*)/tree/(?P<tree_id>[a-z0-9]*)\?path=(?P<path>.*)$',
+    (r'^(?P<project>.*)/tree/(?P<tree_id>.*)\?path=(?P<path>.*)$',
         'tree.html'),
-    (r'^(?P<project>.*)/blob/(?P<blob_id>[a-z0-9]*)\?path=(?P<path>.*)$',
+    (r'^(?P<project>.*)/blob/(?P<blob_id>.*)\?path=(?P<path>.*)$',
      'blob.html'),
-    (r'^(?P<project>.*)/ref/(?P<ref_id>[a-z0-9]*)$', 'ref.html'),
+    (r'^(?P<project>.*)/ref/(?P<ref_id>.*)$', 'ref.html'),
+    (r'^(?P<project>[^/]*)$', 'project.html'),
+    (r'^(?P<project>.*)/diff/(?P<v1>[a-z0-9]*)/(?P<v2>[a-z0-9]*)/(?P<f>.*)$',
+                                                        'diff.html'),
+    (r'^(?P<project>.*)/ssdiff/(?P<v1>.*)/(?P<v2>.*)/(?P<f>.*)$',
+                                                        'ssdiff.html'),
 )
 
